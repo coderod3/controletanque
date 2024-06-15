16a9616482c1a93e95113d8320bb520957b70fea
BrBoy <88920947+BrBoy@users.noreply.github.com>
2024-06-15 11:04:20 -0300

    nivel.php

--- api/nivel.php
+++ api/nivel.php
@@ -5,18 +5,15 @@ $file = 'data.txt';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (isset($_GET['data'])) {
-        $data = filter_input(INPUT_GET, 'data', FILTER_SANITIZE_NUMBER_FLOAT);
+        $data = filter_input(INPUT_GET, 'data', FILTER_SANITIZE_NUMBER_INT);
         file_put_contents($file, $data);
         echo $data; // Return the data sent in the GET request
     } else {
-        if (file_exists($file)) {
-            $data = file_get_contents($file);
-            echo $data; // Return the stored data
-        } else {
-            $data = 5;
-            file_put_contents($file, $data);
-            echo $data; // Return the default data if no stored data exists
+        if (!file_exists($file)) {
+            file_put_contents($file, '5'); // Initialize with default value
         }
+        $data = file_get_contents($file);
+        echo $data; // Return the stored data
     }
 }
 ?>
