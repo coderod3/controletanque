72232febd783b20dd882c3731c54f780594ea482
BrBoy <88920947+BrBoy@users.noreply.github.com>
2024-06-15 11:05:54 -0300

    getLevelToSite.php

--- api/getLevelToSite.php
+++ api/getLevelToSite.php
@@ -1,22 +1,19 @@
 <?php
 session_start();
 
-$file = 'data.txt';
+$file = 'level.txt';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (isset($_GET['data'])) {
-        $data = filter_input(INPUT_GET, 'data', FILTER_SANITIZE_NUMBER_INT);
-        file_put_contents($file, $data);
-        echo $data; // Return the data sent in the GET request
+    if (isset($_GET['newLevel'])) {
+        $newLevel = filter_input(INPUT_GET, 'newLevel', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        file_put_contents($file, $newLevel);
+        echo $newLevel; // Return the new level sent in the GET request
     } else {
-        if (file_exists($file)) {
-            $data = file_get_contents($file);
-            echo $data; // Return the stored data
-        } else {
-            $data = 5;
-            file_put_contents($file, $data);
-            echo $data; // Return the default data if no stored data exists
+        if (!file_exists($file)) {
+            file_put_contents($file, '0'); // Initialize with default value
         }
+        $level = file_get_contents($file);
+        echo $level; // Return the stored level
     }
 }
 ?>
