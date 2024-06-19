1cb600ea4efe02eac614f643042cd41ecc2a101b
BrBoy <88920947+BrBoy@users.noreply.github.com>
2024-06-19 03:16:20 -0300

    Update getValue.js

--- pages/api/getValue.js
+++ pages/api/getValue.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: 'default',
+  host: 'ep-patient-poetry-a49trlr1-pooler.us-east-1.aws.neon.tech',
+  database: 'verceldb',
+  password: 'pvyQPOlU78nH',
+  port: 5432,
   ssl: {
     rejectUnauthorized: false
   }
