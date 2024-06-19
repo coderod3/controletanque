1f71c4e0b73da7a2666fa48fc0b108a90656c7a2
BrBoy <88920947+BrBoy@users.noreply.github.com>
2024-06-19 03:21:42 -0300

    Update getValue.js

--- pages/api/getValue.js
+++ pages/api/getValue.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     client.release();
 
     if (result.rows.length > 0) {
-      const value = result.rows[0].col1;
+      const value = result.rows[0].col1.value;
       res.status(200).json({ value });
     } else {
       res.status(404).json({ error: "No data found" });
