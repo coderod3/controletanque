d3f862aefd35169d4a873da63d8ee6b29a8973a7
Aarod <88920947+BrBoy@users.noreply.github.com>
2024-06-19 05:55:50 -0300

    Update getValue.js

--- pages/api/getValue.js
+++ pages/api/getValue.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
 
       if (result.rows.length > 0) {
         const currentLevel = result.rows[0].col1;
-        res.status(200).json({ level: currentLevel });
+        res.status(200).json(currentLevel);
       } else {
         res.status(404).json({ error: "No level found" });
       }
