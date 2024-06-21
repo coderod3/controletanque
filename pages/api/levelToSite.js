4dbbbae39fe3ca1231134f1e5e0f12dd23394dd6
Aarod <88920947+BrBoy@users.noreply.github.com>
2024-06-21 18:34:39 -0300

    js

--- pages/api/levelToSite.js
+++ pages/api/levelToSite.js
@@ -1,5 +1,4 @@
-// Load environment variables from .env.development.local file
-require('dotenv').config({ path: '.env.development.local' });
+  require('dotenv').config({ path: '.env.development.local' });
 
 const { Pool } = require('pg');
 
@@ -15,20 +14,18 @@ export default async function handler(req, res) {
     const client = await pool.connect();
 
     if (req.query.level) {
-      // If ?level=<value> is provided in the URL, update the database
       const level = parseInt(req.query.level); // Parse the level parameter to an integer
       const updateQuery = 'UPDATE valores SET col1 = $1';
       await client.query(updateQuery, [level]);
       
-      res.status(200).send(level);
+      res.status(200).json({ level: level });
     } else {
-      // If no ?level=<value> is provided, retrieve the current level from the database
       const selectQuery = 'SELECT col1 FROM valores';
       const result = await client.query(selectQuery);
 
       if (result.rows.length > 0) {
         const currentLevel = result.rows[0].col1;
-        res.status(200).send(currentLevel);
+        res.status(200).json({ level: currentLevel });
       } else {
         res.status(404).json({ error: "No level found" });
       }
