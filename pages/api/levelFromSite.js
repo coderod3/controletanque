6c0202103019ac1756331e0274a0044d38f13c58
Aarod <88920947+BrBoy@users.noreply.github.com>
2024-06-21 19:25:49 -0300

    setwaterlevel func

--- pages/api/levelFromSite.js
+++ pages/api/levelFromSite.js
@@ -11,40 +11,53 @@ const pool = new Pool({
 });
 
 module.exports = async (req, res) => {
-  const newLevel = parseFloat(req.query.newLevel);
+  const newLevel = parseFloat(req.query.level);
   const userAgent = req.query.userAgent;
   const userIP = req.query.userIP;
 
-  if (newLevel && userAgent && userIP) {
-    // Send the response immediately
-    res.status(200).json({ level: newLevel });
-
-    // Log and database operations
+  if (newLevel) {
+    // Store the new level in the database and respond immediately
     try {
       const client = await pool.connect();
-
-      // Retrieve the previous level from the 'valores' table
-      const previousResult = await client.query('SELECT col2 FROM valores');
-      const previousLevel = previousResult.rows.length > 0 ? previousResult.rows[0].col2 : null;
 
       // Update the new level in the 'valores' table
       await client.query('UPDATE valores SET col2 = $1', [newLevel]);
 
-      // Log the information
-      const logEntry = {
-        text: 'INSERT INTO logs (user_ip, user_agent, previous_level, new_level) VALUES ($1, $2, $3, $4)',
-        values: [userIP, userAgent, previousLevel, newLevel]
-      };
-      await client.query(logEntry);
+      res.status(200).json({ level: newLevel });
 
       client.release();
     } catch (error) {
       console.error('Error during database operations:', error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
+
+    // Log the information asynchronously
+    (async () => {
+      if (userAgent && userIP) {
+        try {
+          const client = await pool.connect();
+
+          // Retrieve the previous level from the 'valores' table
+          const previousResult = await client.query('SELECT col2 FROM valores');
+          const previousLevel = previousResult.rows.length > 0 ? previousResult.rows[0].col2 : null;
+
+          // Log the information
+          const logEntry = {
+            text: 'INSERT INTO logs (user_ip, user_agent, previous_level, new_level) VALUES ($1, $2, $3, $4)',
+            values: [userIP, userAgent, previousLevel, newLevel]
+          };
+          await client.query(logEntry);
+
+          client.release();
+        } catch (error) {
+          console.error('Error during database operations:', error);
+        }
+      }
+    })();
   } else {
+    // If no new level is provided, return the current level from the 'valores' table
     try {
       const client = await pool.connect();
-      // If no parameters are provided, return the current level from the 'valores' table
       const result = await client.query('SELECT col2 FROM valores');
       const currentLevel = result.rows.length > 0 ? result.rows[0].col2 : null;
       res.status(200).json({ level: currentLevel });
